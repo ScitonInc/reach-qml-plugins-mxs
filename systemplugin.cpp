cd5dc17dfa9194f55ee5cf245ba0d346d3f93311
cmessineo <cmessineo@reachtech.com>
2014-03-05 15:19:17 -0800

    Updated the project and systemplugin file to reflect the Canvas plugin
    Signed-off-by: cmessineo <cmessineo@reachtech.com>

--- systemplugin.cpp
+++ systemplugin.cpp
@@ -3,15 +3,21 @@
 #include "backlight.h"
 #include "network.h"
 #include "system.h"
-
+#include "context2d.h"
+#include "canvas.h"
 #include <QtDeclarative>
 
 void SystemPlugin::registerTypes(const char *uri)
 {
+    Q_ASSERT(uri == QLatin1String("com.reachtech.systemplugin"));
+    qmlRegisterType<System>(uri, 1, 0, "System");
     qmlRegisterType<Beeper>(uri, 1, 0, "Beeper");
     qmlRegisterType<Backlight>(uri, 1, 0, "Backlight");
     qmlRegisterType<Network>(uri, 1, 0, "Network");
-    qmlRegisterType<System>(uri, 1, 0, "System");
+    qmlRegisterType<Canvas>(uri, 1, 0, "Canvas");
+    qmlRegisterType<Context2D>(uri, 1, 0, "Context2D");
+    qmlRegisterUncreatableType<CanvasImage>(uri, 1, 0, "CanvasImage", QString());
+    qmlRegisterUncreatableType<CanvasGradient>(uri, 1, 0, "Gradient", QString());
 }
 
 Q_EXPORT_PLUGIN2(systemplugin, SystemPlugin);
--- systemplugin.pro
+++ systemplugin.pro
@@ -1,7 +1,7 @@
  TEMPLATE = lib
  CONFIG += qt plugin
  QT += declarative network 
-
+ QT += script
  DESTDIR = lib
  OBJECTS_DIR = tmp
  MOC_DIR = tmp
@@ -10,11 +10,17 @@
             systemplugin.h \
             backlight.h \
             network.h \
-            system.h
+            system.h \
+            canvas.h \
+            canvastimer.h \
+            context2d.h
 
  SOURCES += beeper.cpp \
             systemplugin.cpp \
             backlight.cpp \
             network.cpp \
-            system.cpp
+            system.cpp \
+            canvas.cpp \
+            canvastimer.cpp \
+            context2d.cpp
 
